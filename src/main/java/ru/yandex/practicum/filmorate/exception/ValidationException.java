da5f2ff22945c79a86658c5fa8ce69b4bdff2865
Tim Meneshev <tim130512@ya.ru>
2025-05-13 21:49:21 +0700

    finish task

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class ValidationException extends RuntimeException {
     private String wrongValue;
+
     public ValidationException(String message, String value) {
         super(message);
         this.wrongValue = value;
